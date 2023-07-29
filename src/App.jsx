031acefbcb86ab66498308d4c6ad5c0e51b35ef2
Amado <amadodamis@gmail.com>
2023-07-29 15:50:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import masCuatro from "./img/mascuatro.png"
+
+function App() {
+  
+  return (
+    <>
+     <p>HOLA</p>
+     <img src={masCuatro} alt={''} className={''}/>
+    </>
+  )
+}
+
+export default App
