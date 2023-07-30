32297892015027d08585660998ea6e5834fd59ee
Amado <amadodamis@gmail.com>
2023-07-30 01:41:45 -0300

    add  cartas

--- src/components/game/Deck.jsx
+++ src/components/game/Deck.jsx
@@ -1,16 +1,21 @@
 import "./deck.css"
 import mascuatro from "../../img/Cartas/mascuatro.png"
-
+import unoAmarillo from "../../img/Cartas/Amarillas/1.png"
+import dosAzul from "../../img/Cartas/Azules/2.png"
+import tresVerde from "../../img/Cartas/verdes/3.png"
+import dosRojo from "../../img/Cartas/rojas/2.png"
+import cambiodelado from "../../img/Cartas/rojas/cambiodelado.png"
+import elecColor from "../../img/Cartas/eleccioncolor.png"
 function Deck() {
     return (
         <div className="deck">
             <img src={mascuatro} alt='carta-deck' className={''} />
-            <img src={mascuatro} alt='carta-deck' className={''} />
-            <img src={mascuatro} alt='carta-deck' className={''} />
-            <img src={mascuatro} alt='carta-deck' className={''} />
-            <img src={mascuatro} alt='carta-deck' className={''} />
-            <img src={mascuatro} alt='carta-deck' className={''} />
-            <img src={mascuatro} alt='carta-deck' className={''} />
+            <img src={unoAmarillo} alt='carta-deck' className={''} />
+            <img src={dosAzul} alt='carta-deck' className={''} />
+            <img src={elecColor} alt='carta-deck' className={''} />
+            <img src={tresVerde} alt='carta-deck' className={''} />
+            <img src={dosRojo} alt='carta-deck' className={''} />
+            <img src={cambiodelado} alt='carta-deck' className={''} />
         </div>
     )
 }
--- src/components/game/table.jsx
+++ src/components/game/table.jsx
@@ -1,23 +1,25 @@
 import "./table.css"
-import mazo from "../../img/Cartas/mascuatro.png"
+import mazo from "../../img/Cartas/mazo.png"
+import cartainvertida from "../../img/Cartas/cartainvertida.png"
+import mascuatro from "../../img/Cartas/mascuatro.png"
 
 function Table() {
     return (
         <div className="table">
             <div className='deck-computer'>
-                <img src={mazo} alt={''} className='img-size-deck-computer ' />
-                <img src={mazo} alt={''} className='img-size-deck-computer ' />
-                <img src={mazo} alt={''} className='img-size-deck-computer ' />
-                <img src={mazo} alt={''} className='img-size-deck-computer ' />
-                <img src={mazo} alt={''} className='img-size-deck-computer ' />
-                <img src={mazo} alt={''} className='img-size-deck-computer ' />
-                <img src={mazo} alt={''} className='img-size-deck-computer ' />
+                <img src={cartainvertida} alt={''} className='img-size-deck-computer ' />
+                <img src={cartainvertida} alt={''} className='img-size-deck-computer ' />
+                <img src={cartainvertida} alt={''} className='img-size-deck-computer ' />
+                <img src={cartainvertida} alt={''} className='img-size-deck-computer ' />
+                <img src={cartainvertida} alt={''} className='img-size-deck-computer ' />
+                <img src={cartainvertida} alt={''} className='img-size-deck-computer ' />
+           
             </div>
 
             <div className='mazo-cartas-pozo'>
                 <img src={mazo} alt={''} className={""} />
 
-                <img src={mazo} alt={''} className={''} />
+                <img src={mascuatro} alt={''} className={''} />
             </div>
 
             <div className='buttons-table'>
